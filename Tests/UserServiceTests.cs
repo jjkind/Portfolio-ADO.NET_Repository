7355fbc9ad31cdab9e5552fa10743ca688ee9f66
Jeremy Kindseth <jjkindseth@gmail.com>
2020-03-10 00:50:30 -0700

    Adding project container for unit tests.

---
+++ Tests/UserServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    [TestFixture]
+    public class UserServiceTests
+    {
+        [SetUp]
+        public void UserServiceSetup()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
